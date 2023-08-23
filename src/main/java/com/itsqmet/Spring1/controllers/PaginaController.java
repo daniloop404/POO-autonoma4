5442128d8e6d98a8a4f4bdf8fd3b475e9393fc81
Elryusam <138589857+Elryusam@users.noreply.github.com>
2023-08-23 09:11:41 -0500

    first commit

---
+++ src/main/java/com/itsqmet/Spring1/controllers/PaginaController.java
@@ -0,0 +1,31 @@
+package com.itsqmet.Spring1.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class PaginaController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/contacto")
+    public String contacto(){
+        return "contacto";
+    }
+
+    @GetMapping("/nosotros")
+    public String nosotros(){
+        return "nosotros";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+        return "blog";
+    }
+
+    @GetMapping("/portafolio")
+    public String portafolio(){
+        return "portafolio";
+    }
+}
